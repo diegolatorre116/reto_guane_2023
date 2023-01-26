e82d27dccb13b88a36e4a59103fa6d8bd2753bfa
diegolatorre116 <diegolatorre116@gmail.com>
2023-01-26 10:27:58 -0500

    first_configuration

---
+++ app/config.py
@@ -0,0 +1,43 @@
+import os
+import pathlib
+from functools import lru_cache
+
+
+class BaseConfig:
+    BASE_DIR: pathlib.Path = pathlib.Path(__file__).parent.parent
+
+    DATABASE_URL: str = os.environ.get("DATABASE_URL")
+    DATABASE_CONNECT_DICT: dict = {}
+
+    # 'JWT CONFIG'
+    # SECRET_KEY = os.environ.get("SECRET_KEY")
+    # ALGORITHM = os.environ.get("ALGORITHM")
+    # ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    DATABASE_TEST_URL: str = os.environ.get("DATABASE_TEST_URL")
+
+
+@lru_cache()
+def get_settings():
+    config_cls_dict = {
+        "development": DevelopmentConfig,
+        "production": ProductionConfig,
+        "testing": TestingConfig
+    }
+
+    config_name = os.environ.get("FASTAPI_CONFIG", "development")
+    config_cls = config_cls_dict[config_name]
+    return config_cls()
+
+
+settings = get_settings()
